25e88856c4787d87a0ed5a8d6e448f34fe91feb3
Henrique Mulling <henriquemulling20@gmail.com>
2025-11-02 11:59:33 -0300

    Primeiro commit

---
+++ feitoemaula.c
@@ -0,0 +1,44 @@
+#include <stdbool.h>
+
+bool lemonadeChange(int* bills, int billsSize) {
+    int box = 0;
+    int n5 = 0;
+    int n10 = 0;
+    int n20 = 0;
+
+    for (int i = 0; i <= billsSize; i++) {
+
+        if (bills[i] == 5 || bills[i] == 10) {
+        if (bills[i] == 5) {
+            n5++;
+            box = box + 5;
+        }
+        if (bills[i] == 10 && n5 > 0) {
+            box = box + 10;
+            n10++;
+            box = box - 5;
+            n5--;
+            return true;
+        }
+        }
+        if (bills[i] == 20) {
+            if (n5 > 0 && n10 > 0) {
+            box = box + 20;
+            n20++;           
+            box = box - 15;
+            n10--;
+            n5--;
+            return true;
+            } else {
+                if(n5 > 3) {
+                box = box + 20;
+                n20++;
+                box = box - 15;
+                n5 = n5 - 3;
+                return true;
+            }
+            }
+            }
+        }
+            return false;
+    }
